a37b735d4a0cd6486818d720e89e951698f286cb
Bibin <88414576+bibin567@users.noreply.github.com>
2023-06-17 07:03:06 +0100

    file updated

--- main.tf
+++ main.tf
@@ -5,10 +5,27 @@ provider "aws" {
 resource "aws_security_group" "default" {
   name        = "bibinSG"
   description = "security group"
+  lifecycle {
+    create_before_destroy = true
+  }
 
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
