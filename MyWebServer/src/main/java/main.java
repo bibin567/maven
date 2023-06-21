26c6b0432b113e92d3c7617b79db5871ff97d69d
Bibin <88414576+bibin567@users.noreply.github.com>
2023-06-21 12:48:40 +0100

    new commit

--- MyWebServer/src/main/java/main.java
+++ MyWebServer/src/main/java/main.java
@@ -1,8 +1,28 @@
 package com.example.server;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class MyWebServer {
-    public static void main(String[] args) {
-        System.out.println("Hello, Web Server!");
-        // Your web server implementation code goes here
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create();
+        server.bind(null, 80, 0);
+        server.createContext("/", new HelloHandler());
+        server.start();
+        System.out.println("Server started on port 80");
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello, World!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
