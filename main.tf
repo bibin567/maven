112da9142641beb78af6a9b74a787608bfab9152
Bibin <88414576+bibin567@users.noreply.github.com>
2023-06-21 11:08:45 +0100

    new commit

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_instance" "web" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("~/.ssh/id_rsa")
+    private_key = file(var.private_key_path)  # Update to use the variable
     host        = self.public_ip
   }
 
@@ -63,5 +63,10 @@ resource "aws_instance" "web" {
   tags = {
     Name = var.instance_name
   }
+}
+
+variable "private_key_path" {
+  description = "Path to the private key file"
+  type        = string
 }
 
