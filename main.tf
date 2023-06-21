12b480758f128229c94d6e994746532b15b01ac4
Bibin <88414576+bibin567@users.noreply.github.com>
2023-06-21 01:48:28 +0100

    new commit

--- main.tf
+++ main.tf
@@ -1,49 +1,49 @@
-#provider "aws" {
-#  region = "us-east-1"
-#}
-#
-#resource "aws_security_group" "default" {
-#  name        = "bibinSG1"
-#  description = "security group"
-##  replace     = true
-#
-#  ingress {
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  ingress {
-#    from_port   = 8080
-#    to_port     = 8080
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  ingress {
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#}
-#
-#resource "aws_instance" "web" {
-#  ami           = var.ami_id
-#  instance_type = var.instance_type
-#  key_name      = var.key_pair_name
-#  vpc_security_group_ids = [aws_security_group.default.id]
-#  tags = {
-#    Name = var.instance_name
-#  }
-#
-#}
-#
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "default" {
+  name        = "bibinSG1"
+  description = "security group"
+#  replace     = true
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_pair_name
+  vpc_security_group_ids = [aws_security_group.default.id]
+  tags = {
+    Name = var.instance_name
+  }
+
+}
+
